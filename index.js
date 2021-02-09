470016bed65a2f52d7a86f49a5a695e5b933e153
akaneskmz <vermilion_sounds@yahoo.co.jp>
2021-02-09 14:02:09 +0900

    cookieテスト

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
 
+app.use(cookieParser());
 app.use(express.static('public'));
+
+app.get('/cookie-test', function(req, res) {
+    console.log(req.cookies);
+    res.cookie("c", "as");
+    res.send("ok");
+});
 
 app.get('/send-id', function(req, res) {
     res.json({id: myLiffId});
